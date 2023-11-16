e6847bafa6efa50d804ec095d0e38f7dff14bb2f
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-11-16 12:58:36 +0800

    Make Model, Migration Table Question and TopicQuestion

---
+++ database/migrations/2023_11_16_043020_create_topic_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('topic_questions', function (Blueprint $table) {
+            // $table->id();
+            $table->unsignedBigInteger('topic_id');
+            $table->unsignedBigInteger('question_id');
+            $table->timestamps();
+
+
+            // Definisi foreign key
+            $table->foreign('topic_id')->references('id')->on('topics')->onDelete('cascade');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+            
+            // Kunci gabungan sebagai kunci primer
+            $table->primary(['topic_id', 'question_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('topic_questions');
+    }
+};
