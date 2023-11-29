4decc5e4ebc3cdd0d5090a35afad03e477763927
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-11-29 16:54:26 +0800

    Perbaiki FaqController

--- app/Http/Controllers/API/FaqController.php
+++ app/Http/Controllers/API/FaqController.php
@@ -24,7 +24,14 @@ class FaqController extends Controller
             $questions = Question::search($search)->where('is_status', 1)->get();
 
             // Memuat relasi topics untuk setiap pertanyaan
-            $questions->load('topics');
+            $questions->load('topics', 'user');
+
+            return response()->json([
+                'status_code' => 200,
+                'status' => 'success',
+                'message' => 'Data topik berhasil diambil',
+                'data' => $questions,
+            ], 200);
 
             // Transformasi hasil untuk mencocokkan format yang Anda inginkan
             // $results = $questions->map(function ($question) {
@@ -53,36 +60,22 @@ class FaqController extends Controller
             // });
 
         } else {
-            $combinedResults = Topic::where('is_status', 1)->get();
+            $combinedResults = Topic::where('is_status', 1)->with('user')->get();
 
-            $results = $combinedResults->map(function ($item) {
-                // Assuming $item could be either a Topic or a Question model
-                return [
-                    'topic_id' => $item->id,
-                    'topic_name' => $item->name,
-                    'topic_slug' => $item->slug,
-                    'topic_description' => $item->description,
-                    'topic_image' => $item->image,
-                    'topic_icon' => $item->icon,
-                    'topic_is_status' => $item->is_status,
-                    'topic_created_at' => $item->created_at,
-                    'topic_updated_at' => $item->updated_at,
-                ];
-            });
+            return response()->json([
+                'status_code' => 200,
+                'status' => 'success',
+                'message' => 'Data topik berhasil diambil',
+                'data' => $combinedResults,
+            ], 200);
+
         }
 
-
-        return response()->json([
-            'status_code' => 200,
-            'status' => 'success',
-            'message' => 'Data topik berhasil diambil',
-            'data' => $results,
-        ], 200);
     }
 
     public function topic(String $slug)
     {
-        $topic = Topic::where('is_status', 1)->first();
+        $topic = Topic::where('is_status', 1)->where('slug', $slug)->with('questions.user','user')->first();
 
         if (is_null($topic)) {
             return response()->json([
@@ -93,45 +86,19 @@ class FaqController extends Controller
             ], 404);
         }
 
-        $questions = $topic->questions;
-
-        // Transformasi hasil untuk mencocokkan format yang Anda inginkan
-        // $transformedQuestions = $questions->map(function ($question) {
-        //     $topic = $question->topics->first(); // Asumsikan setiap pertanyaan hanya terkait dengan satu topik
-        //     $likesCount = $question->reviews()->where('likes', 1)->count();
-        //     $dislikesCount = $question->reviews()->where('likes', 0)->count();
-
-        //     return [
-        //         'question_id' => $question->id,
-        //         'question_name' => $question->question,
-        //         'question_slug' => $question->slug,
-        //         'question_answer' => $question->answer,
-        //         'question_likes' => $likesCount,
-        //         'question_dislikes' => $dislikesCount,
-        //         'question_created_at' => $question->created_at,
-        //         'question_updated_at' => $question->updated_at,
-        //         'topic_id' => $topic->id,
-        //         'topic_name' => $topic->name,
-        //         'topic_slug' => $topic->slug,
-        //         'topic_description' => $topic->description,
-        //         'topic_image' => $topic->image,
-        //         'topic_icon' => $topic->icon,
-        //         'topic_is_status' => $topic->is_status,
-        //     ];
-        // });
-
-
         return response()->json([
             'status_code' => 200,
             'status' => 'success',
             'message' => 'Data pertanyaan berhasil diambil',
-            'data' => $questions,
+            'data' => $topic,
         ], 200);
     }
 
     public function question(String $name, String $slug)
     {
-        $questions = Question::where('is_status', 1)->where('slug', $slug)->with('reviews')->get();
+        $questions = Question::where('is_status', 1)->where('slug', $slug)->with('user', 'reviews', 'topics.user')->get();
+        // $likesCount = $question->reviews()->where('likes', 1)->count();
+        // $dislikesCount = $question->reviews()->where('likes', 0)->count();
 
         if (is_null($questions)) {
             return response()->json([
@@ -144,34 +111,30 @@ class FaqController extends Controller
 
 
         // Transformasi hasil untuk mencocokkan format yang Anda inginkan
-        // $transformedQuestions = $questions->map(function ($question) {
-        //     $topic = $question->topics->first(); // Asumsikan setiap pertanyaan hanya terkait dengan satu topik
-        //     $likesCount = $question->reviews()->where('likes', 1)->count();
-        //     $dislikesCount = $question->reviews()->where('likes', 0)->count();
+        $transformedQuestions = $questions->map(function ($question) {
+            // $topic = $question->topics->first(); // Asumsikan setiap pertanyaan hanya terkait dengan satu topik
+            $likesCount = $question->reviews()->where('likes', 1)->count();
+            $dislikesCount = $question->reviews()->where('likes', 0)->count();
 
-        //     return [
-        //         'question_id' => $question->id,
-        //         'question_name' => $question->question,
-        //         'question_slug' => $question->slug,
-        //         'question_answer' => $question->answer,
-        //         'question_likes' => $likesCount,
-        //         'question_dislikes' => $dislikesCount,
-        //         'question_created_at' => $question->created_at,
-        //         'question_updated_at' => $question->updated_at,
-        //         'topic_id' => $topic->id,
-        //         'topic_name' => $topic->name,
-        //         'topic_slug' => $topic->slug,
-        //         'topic_description' => $topic->description,
-        //         'topic_image' => $topic->image,
-        //         'topic_is_status' => $topic->is_status,
-        //     ];
-        // });
+            return [
+                'id' => $question->id,
+                'question' => $question->question,
+                'slug' => $question->slug,
+                'answer' => $question->answer,
+                'likes' => $likesCount,
+                'dislikes' => $dislikesCount,
+                'created_at' => $question->created_at,
+                'updated_at' => $question->updated_at,
+                'topics' => $question->topics,
+                'user' => $question->user
+            ];
+        });
 
         return response()->json([
             'status_code' => 200,
             'status' => 'success',
             'message' => 'Data pertanyaan berhasil diambil',
-            'data' => $questions,
+            'data' => $transformedQuestions,
         ], 200);
     }
 
