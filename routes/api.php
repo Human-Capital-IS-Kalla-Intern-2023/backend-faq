ff38f8dd6da0f66bb63d6194bdda87a62b8f73fe
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-11-29 11:10:46 +0800

    Add Api Login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 use App\Http\Controllers\API\TopicController;
 use App\Http\Controllers\API\FaqController;
 use App\Http\Controllers\API\QuestionController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,22 +18,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-
-
-Route::get('faq/topics', [TopicController::class, 'index']);
-Route::post('faq/topics', [TopicController::class, 'store']);
-Route::get('faq/topics/{slug}', [TopicController::class, 'show']);
-Route::get('faq/topics/edit/{slug}', [TopicController::class, 'edit']);
-Route::put('faq/topics/{slug}', [TopicController::class, 'update']);
-Route::delete('faq/topics/{slug}', [TopicController::class, 'destroy']);
-
-Route::get('faq/questions', [QuestionController::class, 'index']);
-Route::post('faq/questions', [QuestionController::class, 'store']);
-Route::get('faq/questions/{slug}', [QuestionController::class, 'show']);
-Route::put('faq/questions/{slug}', [QuestionController::class, 'update']);
-Route::delete('faq/questions/{slug}', [QuestionController::class, 'destroy']);
-
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
 
 Route::get('faq', [FaqController::class, 'index']);
 Route::get('faq/{name}', [FaqController::class, 'topic']);
@@ -40,6 +27,28 @@ Route::get('faq/{name}/{slug}', [FaqController::class, 'question']);
 
 Route::post('faq/{name}/{slug}/like', [FaqController::class, 'like'])->name('like');
 Route::post('faq/{name}/{slug}/dislike', [FaqController::class, 'dislike'])->name('dislike');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('logout', [UserController::class, 'logout']);
+
+    Route::get('faq/topics', [TopicController::class, 'index']);
+    Route::post('faq/topics', [TopicController::class, 'store']);
+    Route::get('faq/topics/{slug}', [TopicController::class, 'show']);
+    Route::get('faq/topics/edit/{slug}', [TopicController::class, 'edit']);
+    Route::put('faq/topics/{slug}', [TopicController::class, 'update']);
+    Route::delete('faq/topics/{slug}', [TopicController::class, 'destroy']);
+
+    Route::get('faq/questions', [QuestionController::class, 'index']);
+    Route::post('faq/questions', [QuestionController::class, 'store']);
+    Route::get('faq/questions/{slug}', [QuestionController::class, 'show']);
+    Route::put('faq/questions/{slug}', [QuestionController::class, 'update']);
+    Route::delete('faq/questions/{slug}', [QuestionController::class, 'destroy']);
+});
+
+
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
